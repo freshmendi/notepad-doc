c92968c8d3d312452d58a91939163503cef41a3c
freshmendi <DarrenZhang@huatongsilver.com>
2020-04-03 16:00:17 +0800

    doc

--- linux/playbook/roles/nginx/files/install_nginx.sh
+++ linux/playbook/roles/nginx/files/install_nginx.sh
@@ -16,7 +16,7 @@ tar xzvf nginx-1.13.8.tar.gz
 rm -rf nginx-1.13.8.tar.gz
 cd nginx-1.13.8
 #编译nginx 并配置需要安装的模块，编译后可通过nginx -V查看----------------------------------------------------------------------
-./configure --with-http_ssl_module  --with-http_stub_status_module --with-http_flv_module --with-http_gzip_static_module
+./configure --with-http_ssl_module  --with-http_stub_status_module --with-http_flv_module --with-http_gzip_static_module
 #google 内存优化 一般用不到
 #--with-google_perftools_module
 #支持流媒体播放
@@ -27,7 +27,7 @@ cd nginx-1.13.8
 #通过 curl 127.0.0.1/nginx_status访问
 #location /nginx_status {
 #	stub_status on;
-#	access_log   off;
+#	access_log   off;
 #}
 #文件压缩模块
 #--with-http_gzip_static_module
@@ -53,11 +53,15 @@ http {
 #以下2条纺织网络堵塞
 	tcp_nopush on;
 	tcp_nodelay on;
-	log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
-					  '\$status \$body_bytes_sent "\$http_referer" '
-					  '"\$http_user_agent" "\$http_x_forwarded_for" "\$upstream_addr" "\$upstream_response_time"';
+	log_format  main  '\$remote_addr - \$remote_user [\$time_local] "\$request" '
+		  '\$status \$body_bytes_sent "\$http_referer" '
+					 '"\$http_user_agent" "\$http_x_forwarded_for" "\$upstream_addr" "\$upstream_response_time"';
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                     '"$http_user_agent" "$http_x_forwarded_for"';
+
 #access_log & error_log
-	access_log  /var/log/nginx/access.log  main;
+	access_log  /var/log/nginx/access.log  main;
 	error_log /var/log/nginx/error.log crit;
 #上传文件大小100M
 
@@ -93,12 +97,12 @@ http {
 	open_file_cache_valid 30s;
 	open_file_cache_min_uses 2;
 #upsteam(负载均衡服务器设置)----------------------------------------------------------------------
-#1:权重  后端服务器宕机会自动剔除
+#1:权重  后端服务器宕机会自动剔除
 #upstream nbh_supplier {
 #	server 172.30.31.10:8084 weight=1;
 #	server 172.30.31.11:8084 weight=1;
 #}
-#2:ip hash  来自同一个ip的访客固定访问一个后端服务器 利于session共享
+#2:ip hash  来自同一个ip的访客固定访问一个后端服务器 利于session共享
 #upstream nbh_supplier2 {
 #	ip_hash;
 #	server 172.30.31.10:8084;
@@ -114,34 +118,34 @@ http {
 #}
 #server(主机设置)----------------------------------------------------------------------
 server {
-	listen	   80;
-	server_name  ip;
+	listen	   80;
+	server_name  ip;
 #location(url配置)----------------------------------------------------------------------
 location /nginx_status {
 	stub_status on;
 }
-#location ^~  /admin {
+#location ^~  /admin {
 #	proxy_pass	http://10.28.141.34:8080;
 #	proxy_redirect off;
 #	proxy_set_header HOST $host;
 #	proxy_set_header X-Real-IP $remote_addr;
 #	proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-#	rewrite ^(.*)$  https://$host$1 permanent;
+#	rewrite ^(.*)$  https://$host$1 permanent;
 #}
 #防止恶意解析
 #if (\$host !~ ^www/.123/.com$){
 #	rewrite ^(.*) http://www.123.com\$1 permanent
 #}
-#缩放旋转tomcat下图片  需要安装--with-http_image_filter_module
+#缩放旋转tomcat下图片  需要安装--with-http_image_filter_module
 #location ^~ /img {
 #	proxy_pass http://10.6.11.33:8080;
-#	image_filter   resize  150 100;
+#	image_filter   resize  150 100;
 #	image_filter rotate 90;
 #}
 #由Nginx处理静态请求
 #location ~*\.(gif|jpg|jpeg|png)$ {
-#	root   /ftp/www/;
-#	image_filter   resize  150 100;
+#	root   /ftp/www/;
+#	image_filter   resize  150 100;
 #	image_filter rotate 90;
 # }
 #nginx 匹配规则
@@ -160,34 +164,34 @@ location /nginx_status {
 #}
 #location /
 #{
-# 24表示255.255.255.0  16表示255.255.0.0  8表示255.0.0.0
+# 24表示255.255.255.0  16表示255.255.0.0  8表示255.0.0.0
 #	deny 192.168.1.1;
 #	allow 192.168.1.0/24
 #	deny all;
 #}
-#由Nginx处理静态请求  windows server下}需要换行单独一行 否则会报location错误
+#由Nginx处理静态请求  windows server下}需要换行单独一行 否则会报location错误
 location ~*\.(htm|html|js|css|less|json|gif|jpg|jpeg|png|bmp|swf|ioc|rar|zip|txt|flv|mid|doc|ppt|pdf|xls|xlsx|mp3|mp4|wma|apk|zip|rar|map)\$ {
 #root 表示根目录是/ftp/www
-	root   /ftp/www/;
+	root   /ftp/www/;
 #静态文件缓存30天
 #expires 30d;
-	index  index.html index.htm;
+	index  index.html index.htm;
 }
-#alias用法  当客户端请求/request_path/image/cat.png的时候，映射为/local_path/image/cat.png
+#alias用法  当客户端请求/request_path/image/cat.png的时候，映射为/local_path/image/cat.png
 #location /request_path/image/{
 #	alias /local_path/image/;
 #}
 }
 #server {
-#	listen	   443 ssl;
-#	server_name  47.97.214.190 www.yysssddyy.top;
-#	ssl_certificate	  /usr/local/nginx/conf/www/214546962750286.pem;
-#	ssl_certificate_key  /usr/local/nginx/conf/www/214546962750286.key;
+#	listen	   443 ssl;
+#	server_name  47.97.214.190 www.yysssddyy.top;
+#	ssl_certificate	  /usr/local/nginx/conf/www/214546962750286.pem;
+#	ssl_certificate_key  /usr/local/nginx/conf/www/214546962750286.key;
 #location ~*\.(htm|html|js|css|less|json|gif|jpg|jpeg|png|bmp|swf|ioc|rar|zip|txt|flv|mid|doc|ppt|pdf|xls|xlsx|mp3|mp4|wma|apk|zip|rar|map)\$ {
-#	root   /ftp/www/;
+#	root   /ftp/www/;
 #静态文件缓存30天
 #expires 30d;
-#	index  index.html index.htm;
+#	index  index.html index.htm;
 #}
 #}
 #也可以指定conf文件位置 在文件夹中配置上述各个参数
@@ -197,8 +201,8 @@ location ~*\.(htm|html|js|css|less|json|gif|jpg|jpeg|png|bmp|swf|ioc|rar|zip|txt
 #----------------------------------------------------------------------
 EOF
 #启动nginx 并把nginx 加入开机启动
-/usr/local/nginx/sbin/nginx  -c /usr/local/nginx/conf/nginx.conf
-sed -i '5a /usr/local/nginx/sbin/nginx  -c /usr/local/nginx/conf/nginx.conf' /etc/rc.d/rc.local
+/usr/local/nginx/sbin/nginx  -c /usr/local/nginx/conf/nginx.conf
+sed -i '5a /usr/local/nginx/sbin/nginx  -c /usr/local/nginx/conf/nginx.conf' /etc/rc.d/rc.local
 chmod +x /etc/rc.d/rc.local
 #将nginx添加到服务中----------------------------------------------------------------------
 cat >>/etc/init.d/nginx<<EOF
